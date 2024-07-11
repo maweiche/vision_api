49984dfb0dbc948cbc3fb3b3d601a92f169376d7
Matt Weichel <matt@Matts-MBP.home>
2024-07-11 14:08:21 -0400

    added getCollectionsByOwner route

---
+++ src/app/api/getCollectionsByOwner/route.ts
@@ -0,0 +1,46 @@
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { Keypair, Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
+import { SDK } from '@maweiche/react-sdk';
+import base58, * as bs58 from "bs58";
+
+export async function POST(request: Request) {
+    let sdk: SDK;
+    try{
+        const body = await request.json();
+        console.log('body', body);
+        const collectionOwner = new PublicKey(body.collectionOwner);
+        
+
+        // CREATE A curl command with the above body to this endpoint
+        // curl -X POST http://localhost:3000/api/getCollectionsByOwner -H "Content-Type: application/json" -d '{"collectionOwner": "6KuX26FZqzqpsHDLfkXoBXbQRPEDEbstqNiPBKHNJQ9e"}'
+
+
+
+        const keypair1 = process.env.ADMINKEYPAIR as string;
+
+        const admin = Keypair.fromSecretKey(base58.decode(keypair1));
+
+        const adminWallet = new NodeWallet(admin);
+        
+        const connection = new Connection(process.env.RPC!, 'confirmed')
+
+        sdk = new SDK(
+            adminWallet as NodeWallet,
+            connection,
+            { skipPreflight: true},
+            "devnet",
+        );
+        const collection = await sdk.collection.getCollectionByOwner(
+            connection, // connection
+            collectionOwner // owner
+          )
+        console.log('data', collection);
+
+        
+        return new Response(JSON.stringify({
+            collection
+        }), { status: 200 });
+    } catch (error) {
+        return new Response(error as string, { status: 500 });
+    }
+}
---
+++ src/app/api/mint/route.ts
@@ -0,0 +1,69 @@
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { Keypair, Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
+import { SDK } from '@maweiche/react-sdk';
+import base58, * as bs58 from "bs58";
+
+export async function POST(request: Request) {
+    let sdk: SDK;
+    try{
+        console.log('request', request);
+        const body = await request.json();
+        console.log('body', body);
+        const id = body.id;
+        const collectionOwner = new PublicKey(body.collectionOwner);
+        
+
+        // CREATE A curl command with the above body to this endpoint
+        // curl -X POST https://localhost:3000/api/mint -H "Content-Type: application/json" -d '{"id": "1", "collectionOwner": "9p2Zp5Uf5xGJ7rV7t2r1fQ8LwX7c9Zr7v7v7v7v7v7v7", "publicKey": "9p2Zp5Uf5xGJ7rV7t2r1fQ8LwX7c9Zr7v7v7v7v7v7"}'
+
+
+
+        const keypair1 = process.env.ADMINKEYPAIR as string;
+
+        const admin = Keypair.fromSecretKey(base58.decode(keypair1));
+
+        const adminWallet = new NodeWallet(admin);
+
+        const buyer = new PublicKey(body.publicKey);
+        
+        const connection = new Connection(process.env.RPC!, 'confirmed')
+
+        sdk = new SDK(
+            adminWallet as NodeWallet,
+            connection,
+            { skipPreflight: true},
+            "devnet",
+        );
+        const data = await sdk.placeholder.createPlaceholder(
+            admin,
+            collectionOwner,
+            buyer,
+            id,
+        );
+        console.log('data', data);
+
+        const tx = new Transaction(
+            {
+                feePayer: buyer,
+                recentBlockhash: (await connection.getRecentBlockhash()).blockhash,
+            }
+        );
+
+        tx.add(...data.instructions)
+        tx.partialSign(admin);
+        console.log('tx', tx)
+        const serializedTransaction = tx.serialize({
+            requireAllSignatures: false,
+        });
+        console.log('about to serialize transaction')
+        const base64 = serializedTransaction.toString("base64");
+        const base64JSON = JSON.stringify(base64);
+        console.log('base64JSON', base64JSON);
+        return new Response(JSON.stringify({
+            transaction: base64JSON as string,
+            placeholder_mint: data.placeholder_mint.toBase58() as string,
+        }), { status: 200 });
+    } catch (error) {
+        return new Response(error as string, { status: 500 });
+    }
+}
