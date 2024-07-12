79972cc37957a7de6f1c5571ce126e2549dc731a
Matt Weichel <matt@Matts-MBP.home>
2024-07-12 12:58:41 -0400

    added getClaims route to return claimable token ids

--- src/app/api/getClaims/route.ts
+++ src/app/api/getClaims/route.ts
@@ -3,6 +3,7 @@ import { Keypair, Connection, PublicKey, GetProgramAccountsFilter } from '@solan
 import { TOKEN_2022_PROGRAM_ID, getTokenMetadata } from "@solana/spl-token";
 import { SDK } from '@maweiche/react-sdk';
 import base58, * as bs58 from "bs58";
+import { token } from "@coral-xyz/anchor/dist/cjs/utils";
 
 export async function POST(request: Request) {
     let sdk: SDK;
@@ -49,25 +50,24 @@ export async function POST(request: Request) {
             {filters: filters}
         );
 
-        const completedTxns = [];
+        const _tokenIds = [];
 
+        for( let i = 0; i < accounts.length; i++ ) {
             //Parse the account data
-            const parsedAccountInfo:any = accounts[0].account.data;
+            const parsedAccountInfo:any = accounts[i].account.data;
             const mintAddress:string = parsedAccountInfo["parsed"]["info"]["mint"];
             const tokenBalance: number = parsedAccountInfo["parsed"]["info"]["tokenAmount"]["uiAmount"];
     
             const _token_metadata = await getTokenMetadata(sdk.rpcConnection, new PublicKey(mintAddress));
     
             if (_token_metadata!.additionalMetadata.length < 6  || tokenBalance == 0  ) {
-                return new Response(JSON.stringify({
-                    transactions: []
-                }), { status: 200 });
+              continue;
             }
     
             const collection_key = _token_metadata!.additionalMetadata[5][1]
     
             //Log results
-            console.log(`Token Account No. ${0 + 1}: ${accounts[0].pubkey.toString()}`);
+            console.log(`Token Account No. ${i + 1}: ${accounts[i].pubkey.toString()}`);
             console.log(`--Token Mint: ${mintAddress}`);
             console.log(`--Token Balance: ${tokenBalance}`);
             console.log(`--Collection Key: ${collection_key}`);
@@ -84,45 +84,13 @@ export async function POST(request: Request) {
                 console.log('placeholder mint', placeholder_mint);
                 console.log('placeholder token id', token_id);
 
-                const getCollectionUrl = async(collection: PublicKey) => {
-                    const collection_data = await connection.getAccountInfo(collection);
-                    const collection_decode = sdk.program.coder.accounts.decode("Collection", collection_data!.data);
-                    console.log('collection_decode', collection_decode)
-                    return {
-                        url: collection_decode.url,
-                        count: collection_decode.mintCount.toNumber(),
-                        owner: collection_decode.owner,
-                    }
-                }
-                const { url, owner } = await getCollectionUrl(collection);
-                console.log('URL TO POLL: ',`${url}/${token_id}/${buyer.toBase58()}`)
 
-
-                
-                const {tx_signature, nft_mint} = await sdk.nft.createNft(
-                    connection,  // connection: Connection,
-                    process.env.BEARER!, // bearer
-                    admin, // admin
-                    collectionOwner, // collection owner
-                    buyer, // buyer    
-                    placeholder_mint // placeholder mint address
-                ); // returns txn signature and nft mint address
-        
-
-                console.log(`nft mint: ${nft_mint}`);
-
-                console.log(`nft tx url: https://explorer.solana.com/tx/${tx_signature}?cluster=${sdk.cluster}`);
-
-                const _tx_obj = {
-                    tx_signature: tx_signature as string,
-                    nft_mint: nft_mint as string,
-                }
-
-                completedTxns.push(_tx_obj);
+                _tokenIds.push(token_id);
             }
+        }
 
         return new Response(JSON.stringify({
-            transactions: completedTxns
+            tokens: _tokenIds
         }), { status: 200 });
     } catch (error) {
         return new Response(error as string, { status: 500 });
